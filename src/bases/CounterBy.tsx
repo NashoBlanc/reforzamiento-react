61962a5cf203dd63edbea317ec3dfe3e400ecc93
Israel Blanc <iiblanc@CLFIFTECHC02FC1GSMD6N.local>
2023-08-14 17:38:27 -0400

    optimizacion y tipado del state

--- src/bases/CounterBy.tsx
+++ src/bases/CounterBy.tsx
@@ -4,8 +4,13 @@ interface CounterProps {
     initialValue?: number
 }
 
+interface CounterState {
+    counter: number;
+    clicks: number;
+}
+
 export const CounterBy = ({ initialValue = 5 } : CounterProps) => {
-    const [counterState, setCounterState] = useState({
+    const [{counter, clicks}, setCounterState] = useState<CounterState>({
         counter: initialValue,
         clicks: 0
     });
@@ -13,16 +18,16 @@ export const CounterBy = ({ initialValue = 5 } : CounterProps) => {
  
     const handleClick = (value: any)  => {
     // el parentetis antes de las llaves indica que vaas a retornar un obj
-        setCounterState(prev => ({
-            counter: prev.counter + value,
-            clicks: prev.clicks + value
+        setCounterState(({counter, clicks}) => ({
+            counter: counter + value,
+            clicks: clicks + value
         }))
     }
 
     return (
         <>
-            <h1>CounterBy: {counterState.counter} </h1>
-            <h1>Clicks: {counterState.clicks} </h1>
+            <h1>CounterBy: {counter} </h1>
+            <h1>Clicks: {clicks} </h1>
             <button onClick={() => handleClick(1)}>+1</button>
             <button onClick={() => handleClick(5)}>+5</button>
         </>
